89f4cc883e7f425864cb332f87604ef0f300c667
RaikkoP <raikko.prants@gmail.com>
2023-12-19 19:11:22 +0200

    Added basic mouse hover flashlight

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Welcome from './components/Welcome.vue'
+import {ref, onMounted, onUnmounted} from 'vue'
+let posX = ref('');
+let posY = ref('');
+  
+
+const trackMousePosition = (e: MouseEvent) => {
+  posX.value = e.clientX -50 + 'px';
+  posY.value = e.clientY -50 + 'px';
+  console.log(posX.value, posY.value)
+}
+onMounted(() => {
+  window.addEventListener('mousemove', trackMousePosition);
+})
+onUnmounted(() => {
+  window.removeEventListener('mousemove', trackMousePosition);
+})
+
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="mouseCircle" :style="{'background': `radial-gradient(circle at ${posX} ${posY}, transparent 10%, rgba(0,0,0,0.8) 80%)`}"/>
+  <Welcome />
+  
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.mouseCircle {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
 }
 </style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="center-title">
+        <h1 class="title">Welcome to my E-Portfolio</h1>
+    </div>
+</template>
+
+<style scoped>
+.center-title {
+   position: absolute;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+}
+.title {
+    color: rgb(131, 130, 130);
+    font-size: 62px;
+}
+
+</style>
