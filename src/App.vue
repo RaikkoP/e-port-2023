a3a0a78cd0ca53331f2583d1f2547c585dd5e54c
RaikkoP <raikko.prants@gmail.com>
2024-01-07 18:22:04 +0200

    Finished it

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <script setup lang="ts">
-import Welcome from './components/Welcome.vue'
-import Introduction from './components/Introduction.vue'
-import AboutMe from './components/Aboutme.vue'
+import CV from './components/CV.vue';
 </script>
 
-
 <template>
-    <div class="background">
-        <Welcome />
-        <Introduction />
-        <AboutMe />
-        <div>
-            <h1>Test</h1>
-        </div>
-    </div>
+    <CV/>
 </template>
 
 <style scoped>
-.background {
-    background-image:
-    linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(174, 27, 137, 0.73), rgba(117, 19, 93, 0.73));  
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-size: cover;
-}
 </style>
---
+++ src/components/CV.vue
@@ -0,0 +1,499 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="intro">
+        <div class="info">
+            <div class="info-text">
+                <h1>Frontend web and mobile developer, and nature explorer</h1>
+                <p>
+                    I am a <span class="highlight">Junior Software Developer</span> hailing from the vibrant city of Tartu.
+                    My primary areas of expertise
+                    lie
+                    in <span class="highlight">JavaScript, Java, and Python.</span> I am currently on the hunt for a <span
+                        class="highlight">junior developer role or an internship
+                        that allows me to utilize my skills</span> and make a tangible impact on a dynamic team.
+                </p>
+                <div class="socials">
+                    <a href="https://www.linkedin.com/in/raikko-prants-95637b286/" target="_blank"><i
+                            class="fa-brands fa-linkedin"></i></a>
+                    <a href="https://github.com/RaikkoP" target="_blank"><i class="fa-brands fa-square-github"></i></a>
+                    <a href="https://www.facebook.com/profile.php?id=100011192951093" target="_blank"><i
+                            class="fa-brands fa-square-facebook"></i></a>
+                </div>
+            </div>
+            <div class="image-container">
+                <img src="../assets/20230909_205512.jpg" />
+            </div>
+        </div>
+    </div>
+
+    <div class="projects">
+        <a href="https://github.com/RaikkoP/ts-taskmanager/tree/main" target="_blank">
+            <div class="project-container">
+                <div class="label">
+                    <h3>React</h3>
+                    <h3>TypeScript</h3>
+                </div>
+                <div class="project-info">
+                    <h2>Taskmanager</h2>
+                    <p>
+                        Small project I made using TypeScript and React, to test out the differences between JavaScript and
+                        TypeScript. App uses local storage and is a To-Do list app.
+                    </p>
+                </div>
+                <div class="project-link">
+                    <p><i class="fa-solid fa-link"></i> View Project</p>
+                </div>
+            </div>
+        </a>
+        <a href="https://github.com/RaikkoP/BibleShorts" target="_blank">
+            <div class="project-container">
+                <div class="label">
+                    <h3>Java</h3>
+                    <h3>Python</h3>
+                </div>
+                <div class="project-info">
+                    <h2>Bible Quote Video Maker</h2>
+                    <p>
+                        Java maven project that calls a bible api, get the data from it and passes it into a Python Flask
+                        server
+                        to
+                        be transformed into a short video.
+                    </p>
+                </div>
+                <div class="project-link">
+                    <p><i class="fa-solid fa-link"></i> View Project</p>
+                </div>
+            </div>
+        </a>
+        <a href="https://github.com/RaikkoP/EmojiForum" target="_blank">
+            <div class="project-container">
+                <div class="label">
+                    <h3>Angular</h3>
+                    <h3>TypeScript</h3>
+                    <h3>Prisma</h3>
+                    <h3>Node.js</h3>
+                </div>
+                <div class="project-info">
+                    <h2>EmojiForum</h2>
+                    <p>
+                        A quick full-stack application that I wanted to try and make using Angular and Prisma. It allows a
+                        user
+                        to
+                        register, login , make posts, see posts and update their profile picture.
+                    </p>
+                </div>
+                <div class="project-link">
+                    <p><i class="fa-solid fa-link"></i> View Project</p>
+                </div>
+            </div>
+        </a>
+        <a href="https://github.com/RaikkoP/DragAndDropTaskmanager" target="_blank">
+            <div class="project-container">
+                <div class="label">
+                    <h3>React</h3>
+                    <h3>JavaScript</h3>
+                </div>
+                <div class="project-info">
+                    <h2>Drag and Drop</h2>
+                    <p>
+                        Wanted to test out making a drag and drop system using only React's built in features without any
+                        external libraries.
+                    </p>
+                </div>
+                <div class="project-link">
+                    <p><i class="fa-solid fa-link"></i> View Project</p>
+                </div>
+            </div>
+        </a>
+        <a href="https://github.com/RaikkoP/ShoutIntoTheVoid" target="_blank">
+            <div class="project-container">
+                <div class="label">
+                    <h3>TypeScript</h3>
+                    <h3>Sqlite</h3>
+                    <h3>Vuejs</h3>
+                </div>
+                <div class="project-info">
+                    <h2>Shout Into The Void</h2>
+                    <p>
+                        Group project, that is styled like a retro spaceship, the application is a work in progress but
+                        should
+                        show a word cloud that is retrieved from the database. All entries in the database are user
+                        submitted.
+                    </p>
+                </div>
+                <div class="project-link">
+                    <p><i class="fa-solid fa-link"></i> View Project</p>
+                </div>
+            </div>
+        </a>
+        <a href="https://github.com/RaikkoP/Puurmani-WP" target="_blank">
+            <div class="project-container">
+                <div class="label">
+                    <h3>PHP</h3>
+                    <h3>WordPress</h3>
+                </div>
+                <div class="project-info">
+                    <h2>Puurmani WP</h2>
+                    <p>
+                        Developing a custom WordPress plugin for my clients school, so that they have more customization
+                        options. Plugin works with Elementor.
+                    </p>
+                </div>
+                <div class="project-link">
+                    <p><i class="fa-solid fa-link"></i> View Project</p>
+                </div>
+            </div>
+        </a>
+    </div>
+
+    <div class="aboutme">
+        <div class="me">
+            <h1>
+                I'm Raikko a software developer from Tartu, where I dream to design the future.
+            </h1>
+            <p>
+                My primary areas of expertise lie
+                in <span class="hightlight">JavaScript, Java, and Python.</span> I am currently on the hunt for a <span
+                    class="highlight">junior developer role or an internship</span>
+                that allows me to utilize my skills and make a tangible impact on a dynamic team.
+
+                My ideal work environment is one that encourages in-person collaboration and operates within a structured,
+                nurturing framework. However, <span class="hightlight">I am flexible and adaptable, able to transition
+                    smoothly between in-office and
+                    remote work settings.</span>
+
+                One of my core strengths is my <span class="highlight">ability to work effectively in teams.</span> I
+                believe that collaboration breeds
+                innovation and that a team is only as strong as its members. <span class="highlight">My strong
+                    problem-solving abilities</span> complement
+                this skill, enabling me to tackle complex issues head-on and devise efficient solutions.
+
+                I am excited about the future and am <span class="highlight">ready to bring my dedication, enthusiasm, and
+                    technical prowess to a
+                    forward-thinking company</span>. Let's build something amazing together!
+            </p>
+            <i style="font-size: 28px; margin-top: 20px;" class="fa-solid fa-phone"></i>
+            <p>5694 7941</p>
+            <br>
+            <i style="font-size: 28px; margin-top: 5px;" class="fa-solid fa-envelope"></i>
+            <p>raikko.prants@gmail.com</p>
+            <div class="socials">
+                <a href="https://www.linkedin.com/in/raikko-prants-95637b286/" target="_blank"><i
+                        class="fa-brands fa-linkedin"></i></a>
+                <a href="https://github.com/RaikkoP" target="_blank"><i class="fa-brands fa-square-github"></i></a>
+                <a href="https://www.facebook.com/profile.php?id=100011192951093" target="_blank"><i
+                        class="fa-brands fa-square-facebook"></i></a>
+            </div>
+        </div>
+        <div class="education-work">
+            <div class="education">
+                <div class="section-title">
+                    <i class="fa-solid fa-school"></i>
+                    <p>Education</p>
+                </div>
+                <div class="list">
+                    <div class="list-item">
+                        <h2>Iisaku Gümnaasium</h2>
+                        <h3>Primary Education</h3>
+                        <h4>2008 - 2017</h4>
+                    </div>
+                    <div class="list-item">
+                        <h2>Iisaku Gümnaasium</h2>
+                        <h3>Secondary Education</h3>
+                        <h4>2017 - 2021</h4>
+                    </div>
+                    <div class="list-item">
+                        <h2>VOCO</h2>
+                        <h3>Software Development</h3>
+                        <h4>2023 - ...</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="work">
+                <div class="section-title">
+                    <i class="fa-solid fa-briefcase"></i>
+                    <p>Work</p>
+                </div>
+                <div class="work-list">
+                    <div class="list-item">
+                        <h2>Omniva</h2>
+                        <h3>Letter Carrier</h3>
+                        <h4>2020 - 2021</h4>
+                    </div>
+                    <div class="list-item">
+                        <h2>Foxway OÜ</h2>
+                        <h3>Electronics Technician</h3>
+                        <h4>2021 - 2022</h4>
+                    </div>
+                    <div class="list-item">
+                        <h2>Puurmani Mansion School</h2>
+                        <h3>Website Technician</h3>
+                        <h4>2022 - 2023</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p style="text-align: center; margin-top: 20px;">&copy Raikko Prants 2024</p>
+    </footer>
+</template>
+
+<style scoped>
+
+.intro {
+    height: 70vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+}
+
+.info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.info-text {
+    width: 50%;
+}
+
+.info-text h1 {
+    font-size: 48px;
+    margin-bottom: 20px;
+}
+
+.info-text p {
+    font-size: 25px;
+}
+
+.image-container {
+    height: 400px;
+    width: 300px;
+    margin-left: 50px;
+}
+
+.image-container img {
+    height: 400px;
+    width: 300px;
+    border-radius: 40px;
+    border: 1px solid black;
+    rotate: 5deg;
+}
+
+.socials {
+    margin-top: 30px;
+    font-size: 48px;
+}
+
+.socials i {
+    color: rgb(179, 179, 179);
+    margin-right: 10px;
+}
+
+.socials i:hover {
+    color: rgb(153, 153, 153);
+    transform: scale(1.1);
+    cursor: pointer;
+}
+
+.highlight {
+    color: rgb(111, 111, 253);
+}
+
+.projects {
+    height: 80vh;
+    width: 80%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: auto;
+    margin-right: auto;
+    flex-wrap: wrap;
+}
+
+.projects a {
+    text-decoration: none;
+}
+
+.label {
+    display: flex;
+    opacity: 0.6;
+}
+
+.label h3 {
+    color: rgb(76, 76, 76);
+    padding: 10px;
+    margin-right: 5px;
+    background-color: rgb(251, 251, 145);
+    border-radius: 40px;
+}
+
+.project-container {
+    width: 400px;
+    height: 250px;
+    padding: 20px;
+    margin: 20px;
+    border-radius: 30px;
+}
+
+.project-container:hover {
+    background-color: rgb(248, 248, 248);
+    transition: all .4s ease;
+    -webkit-transition: all .4s ease;
+    cursor: pointer;
+}
+
+.project-container:hover .project-link {
+    color: rgb(103, 160, 18);
+    transition: all .4s ease;
+    -webkit-transition: all .4s ease;
+}
+
+.project-container:hover .project-info {
+    opacity: 1;
+    transition: all .4s ease;
+    -webkit-transition: all .4s ease;
+}
+
+.project-container:hover .label {
+    opacity: 1;
+    transition: all .4s ease;
+    -webkit-transition: all .4s ease;
+}
+
+.project-info {
+    height: 60%;
+    opacity: 0.6;
+}
+
+.project-info p {
+    text-align: justify;
+    color: rgb(107, 107, 107);
+}
+
+.project-info h2 {
+    color: rgb(76, 76, 76);
+    margin-bottom: 20px;
+    margin-top: 10px;
+}
+
+.project-link {
+    height: 20%;
+}
+
+.aboutme {
+    width: 80%;
+    color: rgb(76, 76, 76);
+    margin-top: 2em;
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.me {
+    width: 60%;
+    padding: 20px;
+}
+
+.me h1 {
+    font-size: 50px;
+}
+
+.me p {
+    text-align: justify;
+    font-size: 28px;
+}
+
+.education-work {
+    width: 40%;
+}
+
+.education {
+    height: 400px;
+    border: 3px solid rgb(231, 231, 231);
+    border-radius: 20px;
+    margin: 10px;
+    padding: 20px;
+}
+
+.section-title {
+    text-align: center;
+    font-size: 28px;
+}
+
+.list {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.list-item {
+    width: 90%;
+    margin: 20px;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.work {
+    height: 400px;
+    border: 3px solid rgb(231, 231, 231);
+    border-radius: 20px;
+    margin: 10px;
+    padding: 20px;
+}
+
+@media only screen and (max-width: 600px) {
+    .intro {
+        height: 100%;
+    }
+    .info {
+        flex-direction: column;
+        width: 90%;
+    }
+    .info-text {
+        width: 100%;
+    }
+    .image-container {
+        margin-top: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .image-container img{
+        rotate: 0deg;
+    }
+    .projects {
+        height: 100%;
+    }
+    .aboutme {
+        flex-direction: column;
+    }
+    .me {
+        width: 90%;
+        padding: 10px;
+    }
+    .education-work {
+        width: 90%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .education {
+        height: 100%;
+    }
+    .work {
+        height: 100%;
+    }
+}
+</style>
